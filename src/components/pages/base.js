dd58ddbb99b5c2d6b225905b83e05c9c596daff2
Luis Choma <luisp.choma@gmail.com>
2021-08-27 03:26:16 -0300

    feat: create Base component

---
+++ src/components/pages/base.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+import './base.scss';
+
+import {
+  Header,
+  Footer
+} from '../atoms'
+
+import { SURVEY_CONFIG } from '../../survey-config/survey-config';
+
+import { useStep } from '../hooks/use-step';
+
+export const initialState = {
+  1: {
+    name: '',
+    email: ''
+  }
+}
+
+export const Base = () => {
+  const { step, callbacks } = useStep()
+
+  const Component = SURVEY_CONFIG[step].component
+
+  const [state, setState] = useState(initialState)
+
+  console.log('stateeeee >>> ', state)
+  return (
+    <div className="base">
+      <div className="base__header">
+        <Header actualPage={step} lastPage={Object.keys(SURVEY_CONFIG).length} />
+      </div>
+
+      <div className="base__content">
+        {<Component state={state[step]} handleChange={([name, email]) => { setState({ ...state, [step]: { name, email } }) }}></Component>}
+      </div>
+
+      <div className="base__footer">
+        <Footer>
+          {SURVEY_CONFIG[step].footer.map((Button, index) => {
+            return (
+              <Button
+                key={index}
+                handleClick={callbacks[index]}
+                label={SURVEY_CONFIG[step].footerLabels[index]}
+                buttonStatus={SURVEY_CONFIG[step].footerStatus[index]} />
+            )
+          })}
+        </Footer>
+      </div>
+    </div >
+  )
+}
