3bf85aa316598b9880518c3a4551dfdf05d37df6
Luis Choma <luisp.choma@gmail.com>
2021-08-27 01:50:28 -0300

    feat: add handleChange method to TextInput

--- src/components/atoms/text-input.js
+++ src/components/atoms/text-input.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 
 import './text-input.scss'
 
-export const TextInput = ({ textLabel, optional = false }) => {
+export const TextInput = ({ textLabel, optional = false, handleChange }) => {
   return (
     <div className="input">
       <label htmlFor={textLabel} className="input__label">
         {textLabel}{optional && <p className="input__label--optional">*</p>}:
       </label>
-      <input type="text" id={textLabel} className="input__textfield" />
+      <input onChange={handleChange} type="text" id={textLabel} className="input__textfield" />
     </div>
   )
 }
 
 TextInput.propTypes = {
   textLabel: PropTypes.string.isRequired,
-  optional: PropTypes.bool
+  optional: PropTypes.bool,
+  handleChange: PropTypes.func
 }
