9570cd52960f4fd2e0a53ba56ff8ff2a53296f85
Luis Choma <luisp.choma@gmail.com>
2021-08-29 23:17:45 -0300

    refactor: remove popup from placeholder file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import React from 'react'
 import './App.scss';
 
 import { Base } from './components/pages';
-import { usePopup } from './components/hooks/use-popup';
 
 const App = () => {
-  const { popupActive, setPopupActive, onClose } = usePopup()
-
-  const stopPropagation = (event) => {
-    event.stopPropagation();
-  }
-
   return (
-    popupActive &&
-    <div onClick={() => setPopupActive(false)} className="popup-container">
-      <div onClick={stopPropagation} className="popup">
-        <Base handleClose={onClose}></Base>
-      </div>
-    </div>
+    <Base />
   )
 }
 
